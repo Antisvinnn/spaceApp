87245386d16a8466416d86823f3246c881eca39b
Vitaly Tochenyy <Antisvinn@yadnex.by>
2021-10-25 18:12:47 +0300

    SCSS fix1

--- src/components/asteroidItem/AsteroidItem.jsx
+++ src/components/asteroidItem/AsteroidItem.jsx
@@ -1,8 +1,15 @@
 import style from './style.module.scss';
+import reactDom from 'react-dom';
 
 const AsteroidItem = ({ name, magnitude, diameter }) => {
+	let portalItem = <span>Hello</span>;
 	return (
-		<div className={style.asteroidContainer}>
+		<div
+			onClick={() => {
+				reactDom.createPortal(portalItem, document.getElementById('portal'));
+			}}
+			className={style.asteroidContainer}
+		>
 			<p className={style.asteroidName}>Name of asteroid - {name}</p>
 			<p className={style.asteroidMagnitude}>Absolute magnitude - {magnitude}</p>
 			<p className={style.asteroidDistance}>Diameter of asteroid - {diameter}</p>
